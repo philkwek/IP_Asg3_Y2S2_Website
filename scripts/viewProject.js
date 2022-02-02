5cf8e430db70aad907464a8d182e19df6f143a73
philkwek <philkwek03@gmail.com>
2022-02-02 17:15:53 +0800

    finished set up of base view projects page
    Need to code liking of posts

--- scripts/viewProject.js
+++ scripts/viewProject.js
@@ -14,11 +14,37 @@ import { getAuth, initializeAuth,
 const auth = getAuth();
 const db = getDatabase();
 
+//References
+var backButton = document.getElementById("backIcon");
+
+if (backButton){
+    backButton.addEventListener("click", function(){
+        window.location = "../html/homepage.html";
+    })
+}
+
 function GetViewProject(){
     var projectId = localStorage.getItem("projectArrayId");
     var latestProjectData = JSON.parse(localStorage.getItem("latestProjectData"));
-    console.log(projectId);
-    console.log(latestProjectData);
+
+    //reference html data points
+    var projectTitle = document.getElementById("projectNameTitle");
+    var likesCount = document.getElementById("likesCount");
+    var creationDate = document.getElementById("creationDate");
+    var creatorName = document.getElementById("creatorName");
+    var companyName = document.getElementById("companyName");
+    var layoutId = document.getElementById("layoutId");
+    var bedroomCount = document.getElementById("bedroomsCount");
+    var furnituresUsed = document.getElementById("furnituresUsed");
+
+    projectTitle.innerHTML = latestProjectData[projectId].nameOfLayout;
+    likesCount.innerHTML = latestProjectData[projectId].likes;
+    creationDate.innerHTML = latestProjectData[projectId].dateCreated;
+    creatorName.innerHTML = latestProjectData[projectId].creator;
+    companyName.innerHTML = latestProjectData[projectId].companyId;
+    layoutId.innerHTML = latestProjectData[projectId].layoutId;
+    bedroomCount.innerHTML = latestProjectData[projectId].noOfBedrooms;
+    furnituresUsed.innerHTML = latestProjectData[projectId].furnitureUsed;
 }
 
 GetViewProject();
