155389d3347a1f3cf7afe872e226cefcf945b089
philkwek <philkwek03@gmail.com>
2022-02-09 08:54:42 +0800

    Search company main backend function done

--- scripts/search.js
+++ scripts/search.js
@@ -16,11 +16,16 @@ const auth = getAuth();
 const db = getDatabase();
 const storage = getStorage();
 
+//Global References
+var companyProjectData;
+var projectKeysArray;
+var pageNumber = 0;
+
 //Button References
 const searchUserButton = document.getElementById("searchUsersBtn");
 const userDiv = document.getElementById("userData");
 const searchCompanyButton = document.getElementById("searchCompanyBtn");
-const companyDiv = document.getElementById("companyDiv");
+const companyDiv = document.getElementById("companyData");
 
 if (searchUserButton){
     searchUserButton.addEventListener("click", function(){
@@ -64,12 +69,165 @@ function SearchUsers(input, emailCheck){
 }
 
 function SearchCompany(companyName){
+    userDiv.style.display = "none";
+    companyDiv.style.display = "";
     const searchQuery = query(ref(db, 'company'), orderByChild("companyName"), equalTo(companyName), limitToFirst(1));
     get(searchQuery).then((snapshot)=>{
         if (snapshot.exists()){
-            console.log(snapshot.val());
+            var data = snapshot.val();
+            data = Object.values(data)[0];
+            document.getElementById("companyName").innerHTML = data.companyName; //inserts name of company
+            const companyId = data.companyId; //get company id for getting company projects
+            GetCompanyProjects(companyId);
+            GetCompanyEmployees(companyId);
+            GetCompanyLogo(companyId);
+
         } else {
             console.log("failed");
+        }
+    });
+}
+
+function GetCompanyLogo(companyId){
+
+}
+
+function GetCompanyProjects(companyId){
+    const searchQuery = query(ref(db, 'projects'), orderByChild("companyId"), equalTo(companyId));
+    get(searchQuery).then((snapshot)=>{
+        if(snapshot.exists()){
+            var data = snapshot.val();
+            companyProjectData = Object.values(data);
+            projectKeysArray = Object.keys(data);
+            localStorage.setItem("companyProjectData", JSON.stringify(data));
+            var projectNumber = 0;
+            document.getElementById("companyProjectCount").innerHTML = companyProjectData.length;
+            for (let i=0; i<companyProjectData.length; i++){ //functions loops through all existing projects, displays first top 8
+                projectNumber += 1;
+                if (projectNumber > 4){
+                    break //ensures function loads only the first 8 projects on screen
+                };
+                //creates reference IDs for innerHTML insertion of data
+                var projectId = "companyProject_" + i;
+                var nameId = "companyNameOfLayout_" + i;
+                var likesId = "companyLikesCount_" + i;
+                var imageId = "companyCoverImage_" + i;
+
+                document.getElementById(projectId).style.display = "inline";
+                document.getElementById(projectId).addEventListener("click", function(){
+                    ViewProject(i);
+                });
+                document.getElementById(nameId).innerHTML = companyProjectData[i].nameOfLayout;
+                
+                if (companyProjectData[i].likes){
+                    document.getElementById(likesId).innerHTML = companyProjectData[i].likes.length;
+                } else {
+                    document.getElementById(likesId).innerHTML = 0;
+                }
+                
+                if (companyProjectData[i].pictures){ 
+                    document.getElementById(imageId).src = "data:image/png;base64," + companyProjectData[i].pictures[0];
+                }else{
+                    document.getElementById(imageId).src = "../resources/icons/Placeholder.png";
+                };
+
+            }
+            //Creates navigation buttons
+            var noOfButtons = Math.ceil(companyProjectData.length/4); //calculate number of nav buttons needed
+            //var noOfButtons = 3;
+            if (noOfButtons > 0){
+                for (let i=0; i<noOfButtons; i++){
+                    //<button type="button" class="btn btn-primary m-1" id="">1</button>
+                    var buttonId = "page" + i;
+                    var buttonName = i + 1;
+                    let btn = document.createElement("button");
+                    btn.innerHTML = buttonName;
+                    if (i==0){
+                    btn.className="btn btn-primary m-1"; //blue color for currently selected page
+                    } else {
+                        btn.className = "btn btn-secondary m-1";
+                    }
+                    btn.id = "pageButton" + i;
+                    btn.addEventListener("click", function(){
+                        nextPage(i);
+                    });
+                    document.getElementById("companyNavigation").appendChild(btn);
+                }
+            }
+        } else {
+            console.log("failed");
+        }
+    });
+}
+
+function nextPage(newPageNumber){ //loads projects based on page number clicked
+    for (let i=0; i<4; i++){ //functions loops through all existing projects, displays that pages 8
+        //creates reference IDs for innerHTML insertion of data
+        var projectId = "companyProject_" + i;
+        document.getElementById(projectId).style.display = "none";
+    }
+    var buttonId = "pageButton" + newPageNumber;
+    var oldButtonId = "pageButton" + pageNumber;
+    document.getElementById(buttonId).className = "btn btn-primary m-1";
+    document.getElementById(oldButtonId).className = "btn btn-secondary m-1";
+    pageNumber = newPageNumber;
+
+    if (pageNumber==0){
+        var projectCount = 0; //current count of project if page number is 1 (or 0 in script terms)
+        var maxProjectNumber = 4; //highest count of projects that can be shown
+    } else {
+        var projectCount = pageNumber * 4; //current count of project
+        var maxProjectNumber = projectCount + 4; //highest count of projects that can be shown
+    }
+
+    var loopCount = -1;
+    for (let i=projectCount; i<companyProjectData.length; i++){ //functions loops through all existing projects, displays that pages 8
+        projectCount += 1;
+        loopCount += 1;
+        if (projectCount > maxProjectNumber){
+            break //ensures function loads only 8 projects
+        };
+        //creates reference IDs for innerHTML insertion of data
+        var projectId = "companyProject_" + loopCount;
+        var nameId = "companyNameOfLayout_" + loopCount;
+        var likesId = "companyLikesCount_" + loopCount;
+        var imageId = "companyCoverImage_" + loopCount;
+
+        document.getElementById(projectId).style.display = "inline";
+        document.getElementById(projectId).addEventListener("click", function(){
+            ViewProject(i);
+        });
+        document.getElementById(nameId).innerHTML = companyProjectData[i].nameOfLayout;
+        
+        if (companyProjectData[i].likes){
+            document.getElementById(likesId).innerHTML = companyProjectData[i].likes.length;
+        } else {
+            document.getElementById(likesId).innerHTML = 0;
+        }
+
+        if (companyProjectData[i].pictures){ 
+            document.getElementById(imageId).src = "data:image/png;base64," + companyProjectData[i].pictures[0];
+        }else{
+            document.getElementById(imageId).src = "../resources/icons/Placeholder.png";
+        };
+
+    }
+}
+
+function GetCompanyEmployees(companyId){
+    const searchQuery = query(ref(db, 'users'), orderByChild("companyId"), equalTo(companyId));
+    get(searchQuery).then((snapshot)=>{
+        if (snapshot.exists()){
+            var data = snapshot.val();
+            data = Object.values(data);
+            var string = ""
+            for (let i = 0; i<data.length; i++){
+                string += data[i].username + "<br>"
+            };
+            document.getElementById("companyDesignerList").innerHTML = string;
+            document.getElementById("companyDesignerCount").innerHTML = data.length;
+        } else {
+            console.log("Failed to get Employees");
         }
     });
 }
